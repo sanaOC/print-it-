d522b786d8886e3df03b1429b6e7c671d6718eff
sana sakka <sana.sakka@gmail.com>
2023-04-13 15:03:47 +0200

    ajout des fleches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,14 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+const fleche_gauche = document.querySelector(".arrow_left") // creer la variable
+fleche_gauche.addEventListener("click", function (event) {       // ajouter le event listener (addEventListener)
+	console.log(event)
+} ) 
+
+const fleche_droite = document.querySelector(".arrow_right")
+fleche_droite.addEventListener("click", function (event) {  	
+	console.log(event)
+} 
+	)
+
